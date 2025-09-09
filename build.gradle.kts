4a750f4c909a736034a84bd088eaad6ca0bfb67a
Everton Ferreira Guedes <evertonferrg@gmail.com>
2025-09-08 23:49:13 -0300

    projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Date
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.5"
@@ -24,17 +26,44 @@ repositories {
 	mavenCentral()
 }
 
+var mapStructVersion = "1.6.3"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mapstruct:mapstruct:$mapStructVersion")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
+
 	compileOnly("org.projectlombok:lombok")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	runtimeOnly("com.h2database:h2")
+
+	annotationProcessor("org.mapstruct:mapstruct-processor:$mapStructVersion")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+tasks.named("build") {
+	doLast{
+		val trigger =file("src/main/resources/trigger.txt")
+		if (!trigger.exists()){ //se o arquivo nao existir ele ira criar
+			trigger.createNewFile()
+		}
+		trigger.writeText(Date().time.toString()) //se existir ira sobrescrever colocando a data e hora atual
+	}
+}
+
+tasks.named<JavaExec>("bootRun"){
+	jvmArgs = listOf("-Xrunjdwp:tranport=dt_socket,server=y,suspend=n,address=*:5005")
 }
 
 tasks.withType<Test> {

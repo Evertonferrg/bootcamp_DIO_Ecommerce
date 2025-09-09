5a1c2e26bac8bfc906d5380a17a18f1c7fe97900
Everton Ferreira Guedes <evertonferrg@gmail.com>
2025-09-09 20:19:45 -0300

    first commit configuracao do docker

---
+++ src/main/java/br/com/dio/storefront/controller/request/ProductSaveRequest.java
@@ -0,0 +1,4 @@
+package br.com.dio.storefront.controller.request;
+
+public record ProductSaveRequest() {
+}
---
+++ src/main/java/br/com/dio/storefront/controller/response/ProductAvailableResponse.java
@@ -0,0 +1,4 @@
+package br.com.dio.storefront.controller.response;
+
+public class ProductAvailableResponse {
+}
